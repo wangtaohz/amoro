02d60c82a1aebf975e4f5422d8f4cad93f568855
wangtao <wangtao3@corp.netease.com>
2023-12-04 13:34:58 +0800

    fix compile error

--- ams/optimizer/flink-optimizer/src/main/java/com/netease/arctic/optimizer/flink/FlinkOptimizer.java
+++ ams/optimizer/flink-optimizer/src/main/java/com/netease/arctic/optimizer/flink/FlinkOptimizer.java
@@ -4,10 +4,6 @@ import com.netease.arctic.optimizer.common.Optimizer;
 import com.netease.arctic.optimizer.common.OptimizerConfig;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.configuration.JobManagerOptions;
-import org.apache.flink.configuration.MemorySize;
-import org.apache.flink.configuration.ReadableConfig;
-import org.apache.flink.configuration.TaskManagerOptions;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.DiscardingSink;
 import org.kohsuke.args4j.CmdLineException;
@@ -25,7 +21,7 @@ public class FlinkOptimizer {
     OptimizerConfig optimizerConfig = new OptimizerConfig(args);
 
     // calculate optimizer memory allocation
-    calcOptimizerMemory(optimizerConfig, env);
+    //    calcOptimizerMemory(optimizerConfig, env);
 
     Optimizer optimizer = new Optimizer(optimizerConfig);
     env.addSource(new FlinkToucher(optimizer.getToucher()))
@@ -45,28 +41,29 @@ public class FlinkOptimizer {
     }
   }
 
-  private static void calcOptimizerMemory(OptimizerConfig config, StreamExecutionEnvironment env) {
-    ReadableConfig configuration = env.getConfiguration();
-    MemorySize jobMemorySize = configuration.get(JobManagerOptions.TOTAL_PROCESS_MEMORY);
-    MemorySize taskMemorySize = configuration.get(TaskManagerOptions.TOTAL_PROCESS_MEMORY);
-    if (jobMemorySize == null || taskMemorySize == null) {
-      // Running locally will not get the configuration.
-      return;
-    }
-    int parallelism = config.getExecutionParallel();
-    int numberOfTaskSlots = configuration.get(TaskManagerOptions.NUM_TASK_SLOTS);
-    int memorySize =
-        jobMemorySize.getMebiBytes()
-            + (parallelism / numberOfTaskSlots) * taskMemorySize.getMebiBytes();
-    if (parallelism % numberOfTaskSlots != 0) {
-      memorySize += taskMemorySize.getMebiBytes();
-    }
-    if (memorySize != config.getMemorySize()) {
-      LOG.info(
-          "Reset the memory allocation of the optimizer to {}, before set is {}",
-          memorySize,
-          config.getMemorySize());
-      config.setMemorySize(memorySize);
-    }
-  }
+  //  private static void calcOptimizerMemory(OptimizerConfig config, StreamExecutionEnvironment
+  // env) {
+  //    ReadableConfig configuration = env.getConfiguration();
+  //    MemorySize jobMemorySize = configuration.get(JobManagerOptions.TOTAL_PROCESS_MEMORY);
+  //    MemorySize taskMemorySize = configuration.get(TaskManagerOptions.TOTAL_PROCESS_MEMORY);
+  //    if (jobMemorySize == null || taskMemorySize == null) {
+  //      // Running locally will not get the configuration.
+  //      return;
+  //    }
+  //    int parallelism = config.getExecutionParallel();
+  //    int numberOfTaskSlots = configuration.get(TaskManagerOptions.NUM_TASK_SLOTS);
+  //    int memorySize =
+  //        jobMemorySize.getMebiBytes()
+  //            + (parallelism / numberOfTaskSlots) * taskMemorySize.getMebiBytes();
+  //    if (parallelism % numberOfTaskSlots != 0) {
+  //      memorySize += taskMemorySize.getMebiBytes();
+  //    }
+  //    if (memorySize != config.getMemorySize()) {
+  //      LOG.info(
+  //          "Reset the memory allocation of the optimizer to {}, before set is {}",
+  //          memorySize,
+  //          config.getMemorySize());
+  //      config.setMemorySize(memorySize);
+  //    }
+  //  }
 }
